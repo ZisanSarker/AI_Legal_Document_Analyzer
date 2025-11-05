cfe9bf5262e995e5068bfb0c0d6ed91d5e0d8cf9
Abir Hasan <skovi281@gmail.com>
2025-11-06 01:40:27 +0600

    feat(add) fraud analysis (messy and inefficient right now

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import morgan from "morgan";
 import extractRouter from "./modules/document/document.route.js";
+import fraudRouter from "./modules/fraud/fraud.route.js";
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/docs", extractRouter);
+app.use("/api/v1/fraud", fraudRouter);
 
 const startServer = () => {
   server = app.listen(PORT, () => {
