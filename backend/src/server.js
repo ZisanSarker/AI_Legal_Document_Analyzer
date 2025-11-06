74b350d968523184e93e4b4077ca5b0b7a3ed2ef
ZisanSarker <sakibuzzamanjisan@gmail.com>
2025-11-07 01:14:29 +0600

    fix(backend): fixes the cors issues and document routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,10 @@ const PORT = process.env.PORT || "5000";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
